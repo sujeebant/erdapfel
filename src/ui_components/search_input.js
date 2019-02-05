e2df35a0ccc81cb8ce8dadeedaf241fc32e2a52f
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-02-05 17:02:09 +0100

    ui_components/search_input: bug fix

--- src/ui_components/search_input.js
+++ src/ui_components/search_input.js
@@ -15,11 +15,10 @@ export default class SearchInput {
 
   /* Singleton */
   static initSearchInput(tagSelector) {
-    if(window.__searchInput) {
-      return window.__searchInput
-    } else {
+    if(! window.__searchInput) {
       window.__searchInput = new SearchInput(tagSelector)
     }
+    return window.__searchInput
   }
 
   static minify() {
